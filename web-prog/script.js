c0b560ea7c35769c76233709546c34e4b79fd5ac
Bogdan <b.oprya2016@gmail.com>
2026-01-24 18:52:21 +0200

    without false answer

--- web-prog/script.js
+++ web-prog/script.js
@@ -1,4 +1,8 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+while (numberOfFilms === 0 || isNaN(numberOfFilms)) {
+  numberOfFilms = +prompt("Введите число больше 0:", "");
+}
 
 let personalMovieDB = {
   count: numberOfFilms,
@@ -9,14 +13,30 @@ let personalMovieDB = {
 };
 
 for (let i = 0; i < 2; i++) {
-  let finishFilmName = prompt(
-    "Назовите один из последних просмотренных фильмов.",
-  );
-  let finishFilmGrade = prompt("На сколько оцените его?");
+  let a, b;
 
-  personalMovieDB.movies[finishFilmName] = finishFilmGrade;
+  while (!a || a.length === 0 || a.length >= 50) {
+    a = prompt("Назовите один из последних просмотренных фильмов.", "");
+
+    if (a === null) {
+      alert("Нельзя отменить ответ!");
+      a = "";
+    }
+
+    if (a.length >= 50) {
+      alert("Слишком длинное название фильма!");
+    }
+  }
+
+  while (b === 0 || isNaN(b)) {
+    b = +prompt("На сколько оцените его?", "");
+
+    if (b === 0 || isNaN(b)) {
+      alert("Введите корректную оценку!");
+    }
+  }
+
+  personalMovieDB.movies[a] = b;
 }
 
-console.log(personalMovieDB)
-
-
+console.log(personalMovieDB);
